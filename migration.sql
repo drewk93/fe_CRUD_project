41b3d7250c85eefa77afa2c278ee2365ae54f6fb
Drew Kullman <agkullman@protonmail.com>
2023-11-13 12:26:50 -0900

    COMMIT 1 - CREATE DB

---
+++ migration.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS planes;
+DROP TABLE IF EXISTS manufacturers;
+
+CREATE TABLE manufacturers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    country VARCHAR(255)
+);
+
+CREATE TABLE planes (
+    id SERIAL PRIMARY KEY,
+    model VARCHAR(255) NOT NULL,
+    tail_number VARCHAR(5) NOT NULL,
+    manufacturer_id INT,
+    CONSTRAINT unique_tail UNIQUE (tail_number),
+    FOREIGN KEY (manufacturer_id) REFERENCES manufacturers (id)
+);
