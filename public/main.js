1cc2bd0d0916acde6b2b7f5ba8180a8e7481db45
Drew Kullman <agkullman@protonmail.com>
2023-11-13 22:52:36 -0900

    Patch, Delete working for planes

--- public/main.js
+++ public/main.js
@@ -1,36 +1,62 @@
 $(document).ready(function(){
+    // READ
     const $planeBtn = $('#planeBtn')
     const $manuBtn = $('#manuBtn')
-    const $createPlaneBtn = $('#createPlaneBtn');
-    const $createManuBtn = $('#createManuBtn')
+    const $fetchResults = $('#fetchResults')
+    // CREATE
+    const $postPlaneBtn = $('#postPlaneBtn');
+    const $postManuBtn = $('#postManuBtn')
+    // UPDATE
+    const $patchPlaneBtn = $("#patchPlaneBtn")
+    const $patchManuBtn = $("#patchManuBtn")
+    // DELETE
+    const $deletePlaneBtn  = $('#deletePlaneBtn')
+    
+    
+    
 
+
+    // READ
     $planeBtn.on('click', getPlane);
     $manuBtn.on('click', getManu)
-    $createPlaneBtn.on('click', createPlane);
-    $createManuBtn.on('click', createManufacturer)
+    // CREATE
+    $postPlaneBtn.on('click', postPlane);
+    $postManuBtn.on('click', postManufacturers)
+    // DELETE
+    $deletePlaneBtn.on('click', deletePlane)
+    // UPDATE
+    $patchPlaneBtn.on('click', patchPlane)
+    $patchManuBtn.on('click', patchManu)
+
 
     // READ FUNCTIONS
 
     function getPlane() {
         console.log("getPlane Working");
+        $fetchResults.empty();
+    
         const url = "http://localhost:3000/planes";
-      
+    
         $.ajax({
-          url,
-          type: "GET",
-          success: function (data) {
-            data.forEach((item, index) => {
-              console.log(item);
-            });
-          },
-          error: function (xhr, status, error) {
-            console.error("Error fetching planes:", error);
-          },
+            url,
+            type: "GET",
+            success: function (data) {
+                data.forEach((item, index) => {
+                    
+                    console.log(item)
+                    $fetchResults.append(`<li class='plane-item'><b>id:</b> ${item.id} <b> model:</b> ${item.model} <b>tail number:</b> ${item.tail_number}</li>`);
+                });
+            },
+            error: function (xhr, status, error) {
+                console.error("Error fetching planes:", error);
+            },
         });
-      }
+    }
       
       function getManu() {
         console.log("getManu Working");
+        $fetchResults.empty()
+
         const url = "http://localhost:3000/manufacturers";
       
         $.ajax({
@@ -39,6 +65,7 @@ $(document).ready(function(){
           success: function (data) {
             data.forEach((item, index) => {
               console.log(item);
+              $fetchResults.append(`<li class='manufacturer-item'><b>id:</b> ${item.id} <b> name:</b> ${item.name} <b>country:</b> ${item.country}</li>`);
             });
           },
           error: function (xhr, status, error) {
@@ -49,10 +76,9 @@ $(document).ready(function(){
 
       // CREATE FUNCTIONS
 
-    function createPlane() {
-        console.log('createPlane Working');
+    function postPlane() {
+        console.log('postPlane Working');
         const url = 'http://localhost:3000/planes';
-    
         const newPlane = {
             model: $('#planeModel').val(),
             tail_number: $('#planeTailNumber').val(),
@@ -65,7 +91,7 @@ $(document).ready(function(){
           contentType: 'application/json',
           data: JSON.stringify(newPlane),
           success: function (data) {
-            console.log('Plane created:', data);
+            console.log('Plane posted:', data);
           },
           error: function (xhr, status, error) {
             console.error('Error creating plane:', error);
@@ -73,9 +99,10 @@ $(document).ready(function(){
         });
       }
 
+
       
-  function createManufacturer() {
-    console.log('createManufacturer Working');
+  function postManufacturers() {
+    console.log('postManufacturers Working');
     const url = 'http://localhost:3000/manufacturers';
 
     const newManufacturer = {
@@ -89,7 +116,7 @@ $(document).ready(function(){
       contentType: 'application/json',
       data: JSON.stringify(newManufacturer),
       success: function (data) {
-        console.log('Manufacturer created:', data);
+        console.log('Manufacturer posted:', data);
       },
       error: function (xhr, status, error) {
         console.error('Error creating manufacturer:', error);
@@ -97,7 +124,77 @@ $(document).ready(function(){
     });
   }
 
-  
+  // PATCH/UPDATE FUNCTIONS
 
+  function patchPlane() {
+    console.log('patchPlane Working');
+    const idToPatch = $('#planeIdToPatch').val();
+    const url = `http://localhost:3000/planes/${idToPatch}`;
+    const patchedPlane = {
+      id: idToPatch,
+      model: $('#patchedPlaneModel').val(),
+      tail_number: $('#patchedPlaneTailNumber').val(),
+      manufacturer_id: $('#patchedPlaneManufacturerId').val(),
+    };
+  
+    try {
+      const response = $.ajax({
+        url,
+        type: 'PATCH',
+        contentType: 'application/json',
+        data: JSON.stringify(patchedPlane),
+      });
+    } catch (error) {
+      console.error('Error updating plane:', error);
+      console.log('Failed to patch plane:', patchedPlane)
+    }
+  }
+
+  function patchManu() {
+    console.log('patchPlane Working');
+    const idToPatch = $('#manuIdToPatch').val()
+    const url = `http://localhost:3000/manufacturers/${idToPatch}`;
+    const patchedManu = {
+        id: idToPatch,
+        name: $('#patchedManuName').val(),
+        country: $('#patchedManuCountry').val()
+    }
+
+    try {
+        const response = $.ajax({
+            url,
+            type: 'PATCH',
+            contentType: 'application/json',
+            data: JSON.stringify(patchedManu),
+        });
+    } catch (error){
+        console.error('Error updating manu:', error)
+        console.error('Failed to patch plane:', patchedManu );
+    }
+  }
+
+
+    // DELETE FUNCTIONS
+ function deletePlane() {
+        console.log('deletePlane Working');
+        const $planeIdToDelete = $('#planeIdToDelete').val()
+        const url = `http://localhost:3000/planes/${$planeIdToDelete}`;
+
+        try {
+        $.ajax({
+            url,
+            type: 'DELETE',
+            success: function (data) {
+                console.log('Plane deleted:', data);
+            },
+            error: function (xhr, status, error) {
+                console.error('Error deleting plane:', error);
+            },
+        });
+        }catch (error){
+            console.error('Error deleting plane:', error)
+          
+        }
+    }
 
 })
--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(express.static('public'))
 
 async function  routes(){
 
+    // GET ROUTES
+
     app.get('/planes', async(req, res, next) => {
         try {
             const result = await pool.query('SELECT * FROM planes')
@@ -32,6 +34,10 @@ async function  routes(){
 
     app.get('/planes/:id', async (req, res, next) => {
         const planeId = parseInt(req.params.id);
+        if (isNaN(planeId) || planeId <= 0) {
+            res.status(400).json({ message: 'Invalid plane ID' });
+            return;
+        }
         try {
         const result = await pool.query('SELECT * FROM planes WHERE id = $1', [planeId]);
         if (result.rowCount === 0) {
@@ -55,6 +61,10 @@ async function  routes(){
 
     app.get('/manufacturers/:id', async (req, res, next) => {
         const manufacturerId = parseInt(req.params.id);
+        if (isNaN(manufacturerId) || manufacturerId <= 0) {
+            res.status(400).json({ message: 'Invalid manufacturer ID' });
+            return;
+        }
         try {
         const result = await pool.query('SELECT * FROM manufacturers WHERE id = $1', [manufacturerId]);
         if (result.rowCount === 0) {
@@ -67,41 +77,52 @@ async function  routes(){
         }
     });
 
+    // POST ROUTES
+
     app.post('/planes', async (req, res, next) => {
+        const newPlane = req.body;
         try {
-        const { model, tail_number, manufacturer_id } = req.body;
-        const result = await pool.query(
-            'INSERT INTO planes (model, tail_number, manufacturer_id) VALUES ($1, $2, $3) RETURNING *',
-            [model, tail_number, manufacturer_id]
-        );
-        res.status(201).json(result.rows[0]);
-        } catch (err) {
-        console.error('Error creating plane:', err.message);
-        res.status(500).json({ error: 'Internal Server Error' });
+            if (!newPlane.model || !newPlane.tail_number || !newPlane.manufacturer_id){
+                res.status(400).json({error: 'Invalid Request'})
+            } else {
+            const result = await pool.query('INSERT INTO planes (model, tail_number, manufacturer_id) VALUES ($1, $2, $3) RETURNING *',
+                [newPlane.model, newPlane.tail_number, newPlane.manufacturer_id]
+            );
+            newPlane.id = result.rows[0].id;
+            res.status(201).json(result.rows[0]);
+            }
+        }catch (err) {
+            next(err)
         }
     });
 
     app.post('/manufacturers', async (req, res, next) => {
+        const newManufacturer = req.body;
         try {
-        const { name, country } = req.body;
-        const result = await pool.query(
-            'INSERT INTO manufacturers (name, country) VALUES ($1, $2) RETURNING *',
-            [name, country]
-        );
-        res.status(201).json(result.rows[0]);
+            if (!newManufacturer.name || !newManufacturer.country) {
+                res.status(400).json({ error: 'Invalid Request' });
+            } else {
+                const result = await pool.query(
+                    'INSERT INTO manufacturers (name, country) VALUES ($1, $2) RETURNING *',
+                    [newManufacturer.name, newManufacturer.country]
+                );
+                newManufacturer.id = result.rows[0].id;
+                res.status(201).json(result.rows[0]);
+            }
         } catch (err) {
-        console.error('Error creating manufacturer:', err.message);
-        res.status(500).json({ error: 'Internal Server Error' });
+            next(err);
         }
     });
 
+    // PATCH ROUTES
+
     app.patch('/planes/:id', async (req, res, next) => {
-        const tailnumber = parseInt(req.params.id);
+        const id = parseInt(req.params.id);
         const updatedPlane = req.body;
         try {
             const result = await pool.query(
                 'UPDATE planes SET model = $1, tail_number = $2, manufacturer_id = $3 WHERE id = $4 RETURNING *',
-                [updatedPlane.model, updatedPlane.tail_number, updatedPlane.manufacturer_id, tailnumber]
+                [updatedPlane.model, updatedPlane.tail_number, updatedPlane.manufacturer_id, id]
             );
 
             // Check if any rows were updated
@@ -118,12 +139,12 @@ async function  routes(){
     });
 
     app.patch('/manufacturers/:id', async (req, res, next) => {
-        const manufacturerId = parseInt(req.params.id);
+        const id = parseInt(req.params.id);
         const updatedManufacturer = req.body;
         try {
         const result = await pool.query(
             'UPDATE manufacturers SET name = $1, country = $2 WHERE id = $3 RETURNING *',
-            [updatedManufacturer.name, updatedManufacturer.country, manufacturerId]
+            [updatedManufacturer.name, updatedManufacturer.country, id]
         );
 
         // Check if any rows were updated
@@ -143,6 +164,10 @@ async function  routes(){
 
     app.delete('/planes/:id', async (req, res, next) => {
         const planeId = parseInt(req.params.id);
+        if (isNaN(planeId) || planeId <= 0) {
+            res.status(400).json({ message: 'Invalid plane ID' });
+            return;
+        }
         try {
           const result = await pool.query('DELETE FROM planes WHERE id = $1 RETURNING *', [planeId]);
           if (result.rowCount === 0) {
@@ -158,6 +183,10 @@ async function  routes(){
 
     app.delete('/manufacturers/:id', async (req, res, next) => {
         const manufacturerId = parseInt(req.params.id);
+        if (isNaN(manufacturerId) || manufacturerId <= 0) {
+            res.status(400).json({ message: 'Invalid manufacturer ID' });
+            return;
+        }
         try {
         const result = await pool.query('DELETE FROM manufacturers WHERE id = $1 RETURNING *', [manufacturerId]);
         if (result.rowCount === 0) {
